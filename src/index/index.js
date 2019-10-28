424e1af39e9000eb420cb9034e753c20afab31b1
chen <13147177623@163.com>
2019-10-28 20:03:14 +0800

    for pull

--- src/index/index.js
+++ src/index/index.js
@@ -1,22 +1,39 @@
-import React,{Component} from 'react';
+import React,{Component,lazy,Suspense} from 'react';
 import ReactDom from 'react-dom';
 
 import './index.less';
 
 export default class Index extends Component{
-
-  render(){
-    // console.log(w,22)
-    return (
-
-      <div className="box">
-       <div className="box1"></div>
-        
-      </div>
-    )
+  constructor(props){
+    super(props);
+    this.state={
+      Search:null,
+    }
 
   }
-
+  addComponent = () => {
+    // import('../search/index.js').then((Search)=>{
+    //   console.log(Search,'Search')
+    //   this.setState({Search:Search.default});
+    // })
+    const Search = lazy(()=>{return import('../search/index.js')});
+    console.log(Search,'Search')
+    this.setState({Search:Search});
+  }
+  render(){
+    // console.log(w,22)
+    const {Search} = this.state;
+    return (
+      <div className="box">
+        <div className="box1">
+          <button onClick={this.addComponent}>点我加载组件</button>
+        </div>
+        <Suspense fallback={null} >
+        {Search &&  <Search />}
+        </Suspense>
+      </div>
+    )
+  }
 }
 
 ReactDom.render(
